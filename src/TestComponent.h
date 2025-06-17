e4baabb4418a00309be39617027ade14ff24af31
Vicente <vicente.martinez@plainconcepts.com>
2025-06-17 23:29:02 +0200

    Deleted TestComponent

--- src/TestComponent.h
+++
@@ -1,9 +0,0 @@
-#pragma once  
-#include "QEGameComponent.h"  
-
-class TestComponent : public QEGameComponent  
-{
-    REFLECTABLE_COMPONENT(TestComponent)
-public:
-    REFLECT_PROPERTY(std::string, Name)
-};
